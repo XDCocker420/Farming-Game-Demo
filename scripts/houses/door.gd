c046616c69e8f7d6c7f86450e012c78cf13e0c70
Daniel.Fass <Fass.Daniel@Student.htlwrn.ac.at>
2024-09-30 15:06:30 +0200

    door fixed, button animation

--- scripts/houses/door.gd
+++ scripts/houses/door.gd
@@ -1,36 +1,38 @@
 extends Area2D
 
-# Variable to track the player when in range
 var player_in_range: CharacterBody2D = null
 var is_open = false
 
 func _ready() -> void:
-# Connect signals for detecting when the player enters or exits the area
 	self.body_entered.connect(_on_body_entered)
 	self.body_exited.connect(_on_body_exited)
+	$button.visible = false
 
 func _on_body_entered(body: Node) -> void:
-# Check if the body is the player
+
 	if body.is_in_group("Player"):
 		player_in_range = body as CharacterBody2D
-# Connect to the player's interact signal
 		player_in_range.interact.connect(_on_player_interact)
 		print("Player is near the door.")
 
+		#Button
+		$button.visible = true
+		$button.play('default')
+
 func _on_body_exited(body: Node) -> void:
-	# Check if the body is the player
 	if body == player_in_range:
-		# Disconnect from the player's interact signal
 		player_in_range.interact.disconnect(_on_player_interact)
 		player_in_range = null
 		print("Player left the door area.")
+		
+		#Button
+		$button.visible = false
+		$button.stop()
 
 func _on_player_interact() -> void:
-	# Called when the player presses the interaction key
 	open_door()
 
 func open_door():
-	# Implement your door opening logic here
 	if !is_open:
 		print("The door opens!")
 		$Door.play("open")
