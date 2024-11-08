2aa236a1964b5a180b9b335a1eb1250716311cd2
Oliver <20210207@htlwrn.ac.at>
2024-11-08 09:07:16 +0100

    optimiziation commit

--- scripts/character/player.gd
+++ scripts/character/player.gd
@@ -2,111 +2,97 @@ extends CharacterBody2D
 
 
 @export var speed = 250  # Movement speed in pixels per second
-var looking_direction = "down"
 @onready var crop_manager = get_node("/root/CropManager")
-@onready var field = get_node("/root/Field")
-
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 var current_field: Field = null
 var selected_crop: String = "carrot"  # Default crop
+var looking_direction = "down"
 
 signal interact
 signal interact2
 signal plant_crop(crop_type, position)
 
 func _ready() -> void:
-	# Add the player to the "Player" group for identification
-	add_to_group("Player")
-	load_state()
-	#Field.plant_crop.connect(CropManager._on_player_plant_crop)
-	# Connect to all fields
-	print("Connecting to fields")
-	for field in get_tree().get_nodes_in_group("fields"):
-		if field is Field:
-			field.player_entered.connect(_on_field_entered)
-			field.player_exited.connect(_on_field_exited)
-	#plant_crop.connect(CropManager._on_player_plant_crop)
+    # Add the player to the "Player" group for identification
+    load_state()
+    #Field.plant_crop.connect(CropManager._on_player_plant_crop)
+    # Connect to all fields
+    print("Connecting to fields")
+    for field in get_tree().get_nodes_in_group("fields"):
+        if field is Field:
+            field.player_entered.connect(_on_field_entered)
+            field.player_exited.connect(_on_field_exited)
+    #plant_crop.connect(CropManager._on_player_plant_crop)
 
 func _input(event: InputEvent) -> void:
-	# Check if the interaction key is pressed
-	if event.is_action_pressed("interact"):
-		interact.emit()
-		print(current_field)
-		if current_field:
-			current_field.try_interact(selected_crop)
-		else:
-			print("You can only plant on farming fields!")
-	if event.is_action_pressed("interact2"):
-		interact2.emit()
+    # Check if the interaction key is pressed
+    if event.is_action_pressed("interact"):
+        interact.emit()
+        print(current_field)
+        if current_field:
+            current_field.try_interact(selected_crop)
+        else:
+            print("You can only plant on farming fields!")
+    if event.is_action_pressed("interact2"):
+        interact2.emit()
 
 func _on_field_entered(field: Field) -> void:
-	print("Field entered: ", field.name)
-	current_field = field
+    print("Field entered: ", field.name)
+    current_field = field
 
 func _on_field_exited(field: Field) -> void:
-	print("Field exited: ", field.name)
-	if current_field == field:
-		current_field = null
+    print("Field exited: ", field.name)
+    if current_field == field:
+        current_field = null
 
 func set_selected_crop(crop_name: String) -> void:
-	selected_crop = crop_name
+    selected_crop = crop_name
 
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * speed
+    var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+    velocity = direction * speed
 
-	if direction.x > 0:
-		looking_direction = "right"
-	elif direction.x < 0:
-		looking_direction = "left"
-	elif direction.y > 0:
-		looking_direction = "down"
-	elif direction.y < 0:
-		looking_direction = "up"
+    if direction.x > 0:
+        looking_direction = "right"
+    elif direction.x < 0:
+        looking_direction = "left"
+    elif direction.y > 0:
+        looking_direction = "down"
+    elif direction.y < 0:
+        looking_direction = "up"
 
-	update_animation(direction)
-	move_and_slide()
+    update_animation(direction)
+    move_and_slide()
  
 
 func update_animation(direction: Vector2):
-	if looking_direction == "right":
-		if direction.x > 0:
-			sprite.play("walk_right")
-		else:
-			sprite.play("idle_right")
-	elif looking_direction == "left":
-		if direction.x < 0:
-			sprite.play("walk_left")
-		else:
-			sprite.play("idle_left")
-	elif looking_direction == "down":
-		if direction.y > 0:
-			sprite.play("walk_down")
-		else:
-			sprite.play("idle_down")
-	elif looking_direction == "up":
-		if direction.y < 0:
-			sprite.play("walk_up")
-		else:
-			sprite.play("idle_up")
+    # Determine animation prefix (walk or idle)
+    var animation_type = "walk" if direction.length() > 0 else "idle"
+    
+    # Combine animation type with current looking direction
+    var animation_name = animation_type + "_" + looking_direction
+    
+    # Play the animation
+    sprite.play(animation_name)
 
 
 ## SAVE FUNCTIONS
 func save_state() -> void:
-	# Save player's position
-	SaveData.data["player_positionX"] = global_position.x
-	SaveData.data["player_positionY"] = global_position.y
+    # Save player's position
+    SaveData.data["player_positionX"] = global_position.x
+    SaveData.data["player_positionY"] = global_position.y
 
 func load_state() -> void:
-	# Load player's position
-	if "player_positionX" in SaveData.data || "player_positionY" in SaveData.data:
-		print("Player position loaded.")
-		global_position = Vector2(
-			SaveData.data["player_positionX"],
-			SaveData.data["player_positionY"]
-		)
+    # Load player's position
+    if "player_positionX" in SaveData.data || "player_positionY" in SaveData.data:
+        print("Player position loaded.")
+        global_position = Vector2(
+            SaveData.data["player_positionX"],
+            SaveData.data["player_positionY"]
+        )
 
 func _notification(what):
-	if what == NOTIFICATION_WM_CLOSE_REQUEST:
-		# Save player state before the game quits
-		save_state()
+    if what == NOTIFICATION_WM_CLOSE_REQUEST:
+        # Save player state before the game quits
+        save_state()
--- scripts/houses/basic_house.gd
+++ scripts/houses/basic_house.gd
@@ -3,35 +3,38 @@ extends TileMapLayer
 
 @onready var e_button: AnimatedSprite2D = $AnimatedSprite2D3
 @onready var door: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player: CharacterBody2D = %Player
+
 var in_area = false
 var open = false
 
 
 func _ready() -> void:
-	e_button.visible = false
+    e_button.visible = false
+    player.interact.connect(_on_player_interact)
 
 
-func _process(delta: float) -> void:
-	if in_area and Input.is_action_just_pressed("interact"):
-		if not open:
-			door.play("open")
-			open = true
-		else:
-			door.play("close")
-			open = false
+func _on_player_interact():
+    if in_area:
+        if not open:
+            door.play("open")
+            open = true
+        else:
+            door.play_backwards("open")
+            open = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		e_button.visible = true
-		in_area = true
+    if body.is_in_group("Player"):
+        e_button.visible = true
+        in_area = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		e_button.visible = false
-		in_area = false
-		
-		if open:
-			door.play("close")
-			open = false
+    if body.is_in_group("Player"):
+        e_button.visible = false
+        in_area = false
+        
+        if open:
+            door.play_backwards("open")
+            open = false
