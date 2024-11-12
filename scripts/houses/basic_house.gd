cd9e82a0a85107f2c7c2c6662c5366f0104ba481
XDCocker420 <20210207@htlwrn.ac.at>
2024-11-12 19:06:22 +0100

    planting works

--- scripts/houses/basic_house.gd
+++ scripts/houses/basic_house.gd
@@ -10,31 +10,31 @@ var open = false
 
 
 func _ready() -> void:
-	e_button.visible = false
-	player.interact.connect(_on_player_interact)
+    e_button.visible = false
+    player.interact.connect(_on_player_interact)
 
 
 func _on_player_interact():
-	if in_area:
-		if not open:
-			door.play("open")
-			open = true
-		else:
-			door.play_backwards("open")
-			open = false
+    if in_area:
+        if not open:
+            door.play("open")
+            open = true
+        else:
+            door.play_backwards("open")
+            open = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		e_button.visible = true
-		in_area = true
+    if body.is_in_group("Player"):
+        e_button.visible = true
+        in_area = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		e_button.visible = false
-		in_area = false
-		
-		if open:
-			door.play_backwards("open")
-			open = false
+    if body.is_in_group("Player"):
+        e_button.visible = false
+        in_area = false
+        
+        if open:
+            door.play_backwards("open")
+            open = false
