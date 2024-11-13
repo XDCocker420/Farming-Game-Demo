c850e64972ff3ce2f85ba6ee48e8a32cc04cfe54
Bill Meixner <20210229@htlwrn.ac.at>
2024-11-13 10:46:16 +0100

    Add storage label to "Scheune" scene adn simple storage method

--- scripts/houses/scheune.gd
+++ scripts/houses/scheune.gd
@@ -1,40 +1,65 @@
 extends TileMapLayer
 
-
 @onready var e_button: AnimatedSprite2D = $AnimatedSprite2D2
 @onready var door: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = %Player
+@onready var storage_label: Label = $StorageLabel
 
 var in_area = false
 var open = false
-
+var storage_amount = 0
+const MAX_STORAGE = 10
 
 func _ready() -> void:
-    e_button.visible = false
-    player.interact.connect(_on_player_interact)
+	e_button.visible = false
+	storage_label.visible = false
+	player.interact.connect(_on_player_interact)
+	player.interact2.connect(_on_player_interact2)
+	
+	if not InputMap.has_action("store_increase"):
+		InputMap.add_action("store_increase")
+		var event = InputEventKey.new()
+		event.keycode = KEY_R
+		InputMap.action_add_event("store_increase", event)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("store_increase") and in_area and open:
+		if storage_amount < MAX_STORAGE:
+			storage_amount += 1
+			update_storage_text()
 
 func _on_player_interact():
-    if in_area:
-        if not open:
-            door.play("open")
-            open = true
-        else:
-            door.play_backwards("open")
-            open = false
+	if in_area:
+		if not open:
+			door.play("open")
+			open = true
+			storage_label.visible = true
+			update_storage_text()
+		else:
+			door.play_backwards("open")
+			open = false
+			storage_label.visible = false
 
+func _on_player_interact2():
+	if in_area and open:
+		if storage_amount > 0:
+			storage_amount -= 1
+			update_storage_text()
+
+func update_storage_text() -> void:
+	storage_label.text = "Lager: %d von %d" % [storage_amount, MAX_STORAGE]
 
 func _on_door_area_body_entered(body):
-    if body.is_in_group("Player"):
-        e_button.visible = true
-        in_area = true
-
+	if body.is_in_group("Player"):
+		e_button.visible = true
+		in_area = true
 
 func _on_door_area_body_exited(body):
-    if body.is_in_group("Player"):
-        e_button.visible = false
-        in_area = false
-        
-        if open:
-            door.play("close")
-            open = false
+	if body.is_in_group("Player"):
+		e_button.visible = false
+		in_area = false
+		
+		if open:
+			door.play_backwards("open")
+			open = false
+			storage_label.visible = false
